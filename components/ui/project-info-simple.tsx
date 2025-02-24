079b063a1fb71f7fb60bf31a48f1ff1c9d6eef37
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2025-02-24 21:47:34 +0000

    minor

--- components/ui/project-info-simple.tsx
+++ components/ui/project-info-simple.tsx
@@ -7,13 +7,13 @@ const ProjectInfoSimple = () => {
       <div className="flex flex-col md:flex-row justify-between gap-6 p-4 md:p-6">
         <div className="flex flex-wrap gap-8">
           <div>
-            <p className="text-secondary text-sm md:text-base">3 Bedroom</p>
-            <p className="text-2xl md:text-3xl">$699,000</p>
+            <p className="text-secondary text-sm md:text-base">2 Bedroom</p>
+            <p className="text-2xl md:text-3xl">$150,000</p>
           </div>
-          <div>
+          {/* <div>
             <p className="text-secondary text-sm md:text-base">5 Bedroom</p>
             <p className="text-2xl md:text-3xl">$799,000</p>
-          </div>
+          </div> */}
         </div>
         <div className="flex flex-wrap gap-8 justify-start md:justify-end">
           <div className="flex flex-col items-center">
@@ -24,7 +24,7 @@ const ProjectInfoSimple = () => {
               height={32}
               className="w-8 h-8 md:w-10 md:h-10"
             />
-            <p className="text-secondary text-sm md:text-base">3 Bedroom</p>
+            <p className="text-secondary text-sm md:text-base">2 Bedroom</p>
           </div>
           <div className="flex flex-col items-center">
             <Image
@@ -34,7 +34,7 @@ const ProjectInfoSimple = () => {
               height={32}
               className="w-8 h-8 md:w-10 md:h-10"
             />
-            <p className="text-secondary text-sm md:text-base">2 Bathroom</p>
+            <p className="text-secondary text-sm md:text-base">2.5 Bathroom</p>
           </div>
           <div className="flex flex-col items-center">
             <Image
